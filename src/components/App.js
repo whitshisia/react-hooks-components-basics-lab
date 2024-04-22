4a90d899edf271d3a386780b16d2c2003fc7229e
whitney shisia <shisiawhitney215@gmail.com>
2024-04-22 15:36:16 +0300

    react

--- src/components/App.js
+++ src/components/App.js
@@ -16,17 +16,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+/* write an <About> component here */
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+        <Home />  
+        <About /> 
     </div>
-  );
+    );
 }
 
 export default App;
